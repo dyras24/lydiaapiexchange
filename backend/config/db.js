b42622a908e783dc28488f7386a3b7ee879ddf4f
dyras24 <hudaagustiar70@gmail.com>
2025-08-24 13:48:24 +0700

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.log("✅ MongoDB Connected...");
+  } catch (err) {
+    console.error("❌ Error connecting to MongoDB:", err.message);
     process.exit(1);
   }
 };
