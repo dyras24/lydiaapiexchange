70a53be8f12d4c7ad772ea10324d3f8cf1fc16d0
dyras24 <hudaagustiar70@gmail.com>
2025-08-24 13:24:54 +0700

    api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,6 @@
-// API fetch ke backend
-async function convertCurrency(from, to, amount) {
-  try {
-    const response = await fetch(
-      `http://localhost:5000/api/market?from=${from}&to=${to}&amount=${amount}`
-    );
-    return await response.json();
-  } catch (err) {
-    console.error("Error fetching API:", err);
-  }
+const API_BASE = "https://lydia-backend.up.railway.app";
+
+export async function getMarketPrice(symbol) {
+  const res = await fetch(`${API_BASE}/market/${symbol}`);
+  return res.json();
 }
