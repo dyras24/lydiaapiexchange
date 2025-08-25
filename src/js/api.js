7cb56d9a182bc3fe8b3f15598c001cfdf875f0f4
dyras24 <hudaagustiar70@gmail.com>
2025-08-25 21:57:52 +0700

    Delete src/js directory

--- src/js/api.js
+++
@@ -1,6 +0,0 @@
-const API_BASE = "https://lydia-backend.up.railway.app";
-
-export async function getMarketPrice(symbol) {
-  const res = await fetch(`${API_BASE}/market/${symbol}`);
-  return res.json();
-}
